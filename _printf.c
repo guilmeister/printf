95276ed2790880cdfedc118df62c8bcf03c31ff7
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-30 00:56:25 +0000

    added edge cases

--- _printf.c
+++ _printf.c
@@ -18,25 +18,32 @@ int _printf(char *format, ...)
 	int (*fptr)(va_list arguments);
 
 	va_start(arguments, format);
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+
 	else
 	{
 		for (i = 0; format[i] != '\0'; i++)
 		{
 			if (format[i] == '%' && format[i + 1] == '%')
 			{	_putchar('%');
-				i+=2;
+				i = i + 2;
 				count++;
 			}
-
+			if (format[i] == '%' && format[i + 1] == ' ')
+			{
+				if (format[i + 2] == '\0')
+					return (0);
+				i = i + 2;
+				count++;
+			}
 			if (format[i] == '%')
 			{
 				i++;
 				fptr = get_percent(format[i]);
 				if (fptr == NULL)
 					return (0);
-				count += fptr(arguments);
+				count = count + fptr(arguments);
 			}
 			else
 			{
